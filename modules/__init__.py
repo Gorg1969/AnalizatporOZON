eacb437b3f390c6e64540805219c824c87496549
Gorg1969 <gorgnik@gmail.com>
2026-09-05 15:41:46 +0500

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,15 @@
+from .detector import SiteDetector
+from .reporter import ReportGenerator
+from .wildberries import WildberriesParser
+from .ozon import OzonParser
+from .security import SecurityManager
+from .worker import TaskManager
+
+__all__ = [
+    'SiteDetector',
+    'ReportGenerator', 
+    'WildberriesParser',
+    'OzonParser',
+    'SecurityManager',
+    'TaskManager'
+]
